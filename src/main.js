167fd2f53929685c6cfb98a3afb73a59a88cb8df
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2019-12-06 09:44:20 -0300

    teste de Login

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 async function getUser(axios, userId) {
   return axios.get("/user/" + userId)
 }
+
+async function postUser(axios, form) {
+  return axios.post("/user/", form )
+}
+
+module.exports.postUser = postUser;
 module.exports.getUser = getUser;
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,6 +1,6 @@
 var axios = require("axios");
 var MockAdapter = require("axios-mock-adapter");
-var { getUser } = require("../src/main");
+var { getUser, postUser } = require("../src/main");
 var chai = require("chai");
 var expect = chai.expect;
 
@@ -9,6 +9,14 @@ var mock = new MockAdapter(axios);
 mock.onGet("/user/13").reply(200, {
   name: "Titi"
 });
+
+const form = {
+  email: "Luis",
+  senha: "12345"
+};
+
+mock.onPost("/user/").reply(200, { form });
+
 describe("main src", () => {
   describe("getUser", () => {
     it("fez o get com sucesso", async () => {
@@ -22,6 +30,21 @@ describe("main src", () => {
       const Response = await getUser(axios, 12).catch(Err => Err.response);
 
       expect(Response.status).to.be.equal(404);
+    });
+  });
+  describe("postUser", () => {
+    it("fez post com sucesso", async () => {
+      const Response = await postUser(axios, form);
+
+      const Login = {
+        email: "Luis",
+        senha: "12345"
+      };
+
+      expect(Response.data.form).to.be.an("object");
+      expect(Response.status).to.be.equal(200);
+      expect(Response.data.form.email).to.be.deep.equal(Login.email);
+      expect(Response.data.form.senha).to.be.deep.equal(Login.senha);
     });
   });
 });
