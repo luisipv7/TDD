0dd35b4f8467f09adba92a2fde66e59f670c35ca
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2019-12-06 09:51:14 -0300

    Teste de login sem sucesso passou

--- test/main.spec.js
+++ test/main.spec.js
@@ -32,6 +32,8 @@ describe("main src", () => {
       expect(Response.status).to.be.equal(404);
     });
   });
+
+  // Teste de Login com sucesso
   describe("postUser", () => {
     it("fez post com sucesso", async () => {
       const Response = await postUser(axios, form);
@@ -45,6 +47,22 @@ describe("main src", () => {
       expect(Response.status).to.be.equal(200);
       expect(Response.data.form.email).to.be.deep.equal(Login.email);
       expect(Response.data.form.senha).to.be.deep.equal(Login.senha);
+    });
+  });
+  // Falha teste de Login com sucesso 
+  describe("postUser", () => {
+    it("não fez post com sucesso", async () => {
+      const Response = await postUser(axios, form);
+
+      const Login = {
+        email: "Titi",
+        senha: "54321"
+      };
+
+      expect(Response.data.form).to.be.an("object");
+      expect(Response.status).to.be.equal(200);
+      expect(Response.data.form.email).to.be.not.deep.equal(Login.email);
+      expect(Response.data.form.senha).to.be.not.deep.equal(Login.senha);
     });
   });
 });
